a71656668cbfdf5e1490a15d46736abeb6680b62
Hunter102 <40743716+Hunter102@users.noreply.github.com>
2023-04-09 14:10:39 -0400

    Added moveNode() and comments on it (not implemented), detailed emergency classes

--- src/main/java/edu/wpi/teamname/navigation/Node.java
+++ src/main/java/edu/wpi/teamname/navigation/Node.java
@@ -1,8 +1,15 @@
 package edu.wpi.teamname.navigation;
 
+import edu.wpi.teamname.database.DataManager;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 public class Node {
 
@@ -92,5 +99,62 @@ public class Node {
   public double calculateHeuristic(Node target) {
     // Heuristic will return distance from target
     return 0;
+  }
+
+  /**
+   * Checks if this.nodeID has switched with given nodeID to swap positions, floor and building in the table
+   * with it in the edge table
+   * also swaps correlating information as well as switching the longNames with eachother in LocationName table
+   * @param move
+   * @return Boolean
+   */
+  public boolean moveNode(Move move) throws SQLException {
+    Connection connection = DataManager.DbConnection();
+    boolean done = false;
+    int swapNodeID = move.getNodeID();
+    String swapLongN = move.getLongName();
+    int thisNode = this.id;
+
+    String getLn = "Select \"longName\" from \"Move\" where \"nodeID\" = " + thisNode;
+    try (PreparedStatement s = connection.prepareStatement(getLn)) {
+      ResultSet rowsUpdated = s.executeQuery();
+      String longN = rowsUpdated.getString("longName");
+    } catch (SQLException e2) {
+      System.out.println("Error getting long name.");
+    }
+
+    /**
+     * Might use sync functions with this feature
+     */
+    //A starting node, B is node being swapped with
+
+    //rowAlocN = select longName from LocationName where longN = longN
+    //rowBlocN = select longName from LocationName where longN = swapLongN
+    //rowAnode = select floor, building from Node where thisNode = thisNode
+    //rowBnode = select floor, building from Node where thisNode = swapNodeID
+
+    //put rowAlocN where longN = swapLongN
+    //put rowAnode where thisNode = swapNodeID
+    //insert nodeID, longN, date into Move
+
+    //put rowBnode where thisNode = thisNode
+    //put rowBlocN where longN = longN
+
+    //insert swapNodeID, swapLongN, date into Move
+
+    String query = "";
+
+    try (PreparedStatement pstmtUpdate = connection.prepareStatement(query)) {
+      int rowsUpdated = pstmtUpdate.executeUpdate();
+      if (rowsUpdated > 0) {
+        System.out.println("successfully updated");
+      } else {
+        System.out.println("not updated");
+      }
+    } catch (SQLException e) {
+      System.out.println("Error updating LocationName record for node ID " + thisNode);
+    }
+
+    return done;
   }
 }
