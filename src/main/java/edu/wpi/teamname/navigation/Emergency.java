9da4efc6df199e77c2973269d52415feec5a506e
Hunter102 <40743716+Hunter102@users.noreply.github.com>
2023-04-09 14:10:39 -0400

    Added moveNode() and comments on it (not implemented), detailed emergency classes

--- src/main/java/edu/wpi/teamname/navigation/Emergency.java
+++ src/main/java/edu/wpi/teamname/navigation/Emergency.java
@@ -6,11 +6,24 @@ public class Emergency {
 
   public Emergency() {}
 
+  /**
+   * sets emergency level status to None
+   * @param e
+   */
   public static void resolveEmergency(Emergency e) {}
 
+  /**
+   * Sets given node to which ever level status it should be based on input
+   * @param n
+   */
   public void updateEmergencyNode(Node n) {}
 
-  public EmergencyStatus status() {
+  /**
+   * Sets given nodes emergency status
+   * @param lvl
+   * @return
+   */
+  public EmergencyStatus status(int lvl) {
     return null;
   }
 }
