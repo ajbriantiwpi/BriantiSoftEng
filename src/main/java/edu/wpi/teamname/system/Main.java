83972661eb36e4e725b15112e8eeec86e0d76d47
jtkardon <jtkardon@gmail.com>
2023-04-09 17:23:17 -0400

    deleted main thorwing exception

--- src/main/java/edu/wpi/teamname/system/Main.java
+++ src/main/java/edu/wpi/teamname/system/Main.java
@@ -1,7 +1,7 @@
 package edu.wpi.teamname.system;
 
 public class Main {
-  public static void main(String[] args) throws SQLException {
+  public static void main(String[] args) {
     // Singleton is in DataManager, function has it so it will only return one connection when
     // called
     App.launch(App.class, args);
