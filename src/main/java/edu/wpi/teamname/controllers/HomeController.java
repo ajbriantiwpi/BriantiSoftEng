ac4bab8becba00b335a76aeb43ed78eeaaeb36c0
ajbriantiwpi <ajbrianti@wpi.edu>
2023-04-10 19:30:16 -0400

    Adding the fxml files

--- src/main/java/edu/wpi/teamname/controllers/HomeController.java
+++ src/main/java/edu/wpi/teamname/controllers/HomeController.java
@@ -4,13 +4,63 @@ import edu.wpi.teamname.system.Navigation;
 import edu.wpi.teamname.system.Screen;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import lombok.Setter;
 
 public class HomeController {
 
-  @FXML MFXButton navigateButton;
+  @FXML MFXButton homeButton;
+  @FXML MFXButton helpButton;
+  @FXML MFXButton mapButton;
+  @FXML MFXButton directionsButton;
+  @FXML MFXButton makeRequestsButton;
+  @FXML MFXButton showRequestsButton;
+  @FXML MFXButton editMapButton;
+  @FXML MFXButton exitButton;
+
+  // test push
+  @Setter private static boolean loggedIn = false;
+  @FXML ImageView imageView;
+  @FXML private AnchorPane rootPane;
+  @FXML MFXButton loginButton;
+  @FXML MFXButton logoutButton;
+
+  private void logout() {
+    loggedIn = false;
+    loginButton.setVisible(true);
+    logoutButton.setVisible(false);
+  }
 
   @FXML
   public void initialize() {
-    navigateButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUEST));
+
+    // set the width and height to be bound to the panes width and height
+    //    imageView.fitWidthProperty().bind(rootPane.widthProperty());
+    //    imageView.fitHeightProperty().bind(rootPane.heightProperty());
+    // this allows for the image to stay at the same size of the rootPane, which is the parent pane
+    // of the Home.fxml
+
+    // Param is EventHandler
+    // Lambda Expression. parameter -> expression
+    // Basically just runs the Navigation.navigate Function
+    // "event" is a parameter, but there is no
+    if (loggedIn) {
+      loginButton.setVisible(false);
+      logoutButton.setVisible(true);
+    } else {
+      loginButton.setVisible(true);
+      logoutButton.setVisible(false);
+    }
+    loginButton.setOnMouseClicked(event -> Navigation.navigate(Screen.LOGIN));
+
+    homeButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+    //    helpButton.setOnMouseClicked(event -> Navigation.navigate(Screen.));
+    mapButton.setOnMouseClicked(event -> Navigation.navigate(Screen.MAP));
+    directionsButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SIGNAGE));
+    makeRequestsButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUEST));
+    showRequestsButton.setOnMouseClicked(event -> Navigation.navigate(Screen.SERVICE_REQUEST_VIEW));
+    editMapButton.setOnMouseClicked(event -> Navigation.navigate(Screen.MAP_EDIT));
+    exitButton.setOnMouseClicked(event -> System.exit(0));
   }
 }
--- src/main/java/edu/wpi/teamname/controllers/LoginController.java
+++ src/main/java/edu/wpi/teamname/controllers/LoginController.java
@@ -1,9 +1,86 @@
 package edu.wpi.teamname.controllers;
 
+import edu.wpi.teamname.database.DataManager;
+import edu.wpi.teamname.navigation.Navigation;
+import edu.wpi.teamname.navigation.Screen;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import java.awt.*;
+import java.sql.SQLException;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.StackPane;
 
 public class LoginController {
+  @FXML MFXButton exit;
+  @FXML AnchorPane rootPane;
+  @FXML StackPane paneOfStuff;
+  String tempPassword;
+  @FXML Label newPassword;
+  @FXML Label success;
+  @FXML MFXButton loginButton;
+  @FXML MFXButton forgotPassword;
+  @FXML TextField loginText;
+  @FXML PasswordField passwordText;
+
+  private static boolean loginPressed(String username, String password) throws SQLException {
+    //    Login user = new Login(username, password);
+    boolean successLog = DataManager.Login(username, password);
+    if (successLog) {
+      HomeController.setLoggedIn(true);
+      Navigation.navigate(Screen.HOME);
+      return true;
+    } else {
+      return false;
+    }
+  }
 
   @FXML
-  public void initialize() {}
+  public void initialize() {
+    newPassword.setVisible(false);
+    success.setText("Username or password\nis incorrect");
+    success.setVisible(false);
+    exit.setOnMouseClicked(event -> System.exit(0));
+    forgotPassword.disableProperty().bind(Bindings.isEmpty(loginText.textProperty()));
+    loginButton.disableProperty().bind(Bindings.isEmpty(loginText.textProperty()));
+    loginButton.disableProperty().bind((Bindings.isEmpty(passwordText.textProperty())));
+    forgotPassword.setOnMouseClicked(
+            event -> {
+              try {
+                tempPassword = forgotPasswordPressed(loginText.getText());
+                newPassword.setText("Your new password is: \n" + tempPassword);
+                newPassword.setVisible(true);
+                paneOfStuff.setDisable(true);
+              } catch (SQLException e) {
+                throw new RuntimeException(e);
+              }
+            });
+    rootPane.setOnMouseClicked(
+            event -> {
+              paneOfStuff.setDisable(false);
+              newPassword.setVisible(false);
+              success.setVisible(false);
+            });
+
+    loginButton.setOnMouseClicked(
+            event -> {
+              try {
+                boolean temp = loginPressed(loginText.getText(), passwordText.getText());
+                if (!temp) {
+                  paneOfStuff.setDisable(true);
+                  success.setVisible(true);
+                  passwordText.clear();
+                }
+              } catch (SQLException e) {
+                throw new RuntimeException(e);
+              }
+            });
+  }
+
+  private String forgotPasswordPressed(String username) throws SQLException {
+    return DataManager.forgotPassword(username);
+  }
 }
