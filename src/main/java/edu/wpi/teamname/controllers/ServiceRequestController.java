ef9ad4cc37c178a6cf006edb0f775175af2d27f8
ajbriantiwpi <ajbrianti@wpi.edu>
2023-04-10 23:00:59 -0400

    made main run properly

--- src/main/java/edu/wpi/teamname/controllers/ServiceRequestController.java
+++ src/main/java/edu/wpi/teamname/controllers/ServiceRequestController.java
@@ -1,18 +1,15 @@
 package edu.wpi.teamname.controllers;
 
-import edu.wpi.teamname.system.Navigation;
-import edu.wpi.teamname.system.Screen;
-import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 
 public class ServiceRequestController {
 
-  @FXML MFXButton backButton;
+  //  @FXML MFXButton backButton;
 
   @FXML
   public void initialize() {
 
-    backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
+    //    backButton.setOnMouseClicked(event -> Navigation.navigate(Screen.HOME));
     ParentController.titleString.set("Service Request");
   }
 }
