0ac409c5da3640ca61f1b00ae98b7e08fd5bf1f5
C-Slat <ctslattery@wpi.edu>
2023-04-11 21:00:39 -0400

    data for ServRequestView imported

--- src/main/java/edu/wpi/teamname/controllers/ServiceRequestViewController.java
+++ src/main/java/edu/wpi/teamname/controllers/ServiceRequestViewController.java
@@ -1,11 +1,51 @@
 package edu.wpi.teamname.controllers;
 
+import edu.wpi.teamname.database.DataManager;
+import edu.wpi.teamname.servicerequest.ServiceRequest;
+import java.sql.SQLException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
+import javafx.scene.control.cell.PropertyValueFactory;
+import org.controlsfx.control.tableview2.FilteredTableColumn;
+import org.controlsfx.control.tableview2.FilteredTableView;
 
 public class ServiceRequestViewController {
 
+  @FXML FilteredTableView table;
+  @FXML FilteredTableColumn requestIDCol;
+  @FXML FilteredTableColumn patientNameCol;
+  @FXML FilteredTableColumn roomNumCol;
+  @FXML FilteredTableColumn requesterIDCol;
+  @FXML FilteredTableColumn requestedAtCol;
+  @FXML FilteredTableColumn requestedForCol;
+  @FXML FilteredTableColumn assignedStaffCol;
+  @FXML FilteredTableColumn statusCol;
+
   @FXML
-  public void initialize() {
+  public void initialize() throws SQLException {
     ParentController.titleString.set("Service Request View");
+
+    ObservableList<ServiceRequest> serviceRequests =
+        FXCollections.observableList(DataManager.getAllServiceRequests());
+    FilteredList<ServiceRequest> serviceRequests1 = new FilteredList<>(serviceRequests);
+    serviceRequests1.predicateProperty().bind(table.predicateProperty());
+    SortedList<ServiceRequest> sortedServiceReq = new SortedList<>(serviceRequests1);
+    table.setItems(sortedServiceReq);
+    requestIDCol.setCellValueFactory(new PropertyValueFactory<ServiceRequest, String>("requestID"));
+    roomNumCol.setCellValueFactory(new PropertyValueFactory<ServiceRequest, String>("roomNumber"));
+    assignedStaffCol.setCellValueFactory(
+        new PropertyValueFactory<ServiceRequest, String>("staffName"));
+    patientNameCol.setCellValueFactory(
+        new PropertyValueFactory<ServiceRequest, String>("patientName"));
+    requestedAtCol.setCellValueFactory(
+        new PropertyValueFactory<ServiceRequest, String>("requestedAt"));
+    requestedForCol.setCellValueFactory(
+        new PropertyValueFactory<ServiceRequest, String>("deliverBy"));
+    statusCol.setCellValueFactory(new PropertyValueFactory<ServiceRequest, String>("status"));
+    requesterIDCol.setCellValueFactory(
+        new PropertyValueFactory<ServiceRequest, String>("requestMadeBy"));
   }
 }
