5a8558605e2025f7bc9c406e48a5305e9bd26f0b
Hunter102 <40743716+Hunter102@users.noreply.github.com>
2023-04-14 14:07:01 -0400

    Medical supplies and table, updated furnature table

---
+++ src/main/java/edu/wpi/teamname/servicerequest/requestitem/MedicalSupply.java
@@ -0,0 +1,70 @@
+package edu.wpi.teamname.servicerequest.requestitem;
+
+import edu.wpi.teamname.database.DataManager;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import lombok.Getter;
+import lombok.Setter;
+
+public class MedicalSupply extends RequestItem{
+    @Getter @Setter String type;
+    @Getter @Setter String accessLvl;
+
+    /**
+     * Creates medical supply object
+     *
+     * @param supplyID
+     * @param name
+     * @param price
+     * @param type
+     * @param accessLvl
+     */
+    public MedicalSupply(int supplyID, String name, float price, String type, String accessLvl){
+        super(supplyID, name, price);
+        this.type = type;
+        this.accessLvl = accessLvl;
+    }
+
+    public MedicalSupply(int id) throws SQLException{
+        super(id);
+        Connection connection = DataManager.DbConnection();
+        String query = "SELECT * FROM \"Flowers\" WHERE \"flowerID\" = ?;";
+
+        String name = null;
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, id);
+            ResultSet rs = statement.executeQuery();
+            while (rs.next()) {
+                super.setItemID(id);
+                super.setName(rs.getString("Name"));
+                setPrice(rs.getFloat("Price"));
+                setType(rs.getString("Type"));
+                setAccessLvl(rs.getString("AccessLevel"));
+            }
+        } catch (SQLException e) {
+            System.out.println("Error retrieving medical supply data: " + e.getMessage());
+            System.out.println(id);
+        }
+    }
+
+    /**
+     * A string of the item
+     *
+     * @return String in format [ <itemID>, <name>, <price>, <type>, <accessLvl>]
+     */
+    public String toString() {
+        return "["
+                + this.getItemID()
+                + ", "
+                + this.getName()
+                + ", "
+                + this.getPrice()
+                + ", "
+                + type
+                + ", "
+                + accessLvl
+                +  "]";
+    }
+}
