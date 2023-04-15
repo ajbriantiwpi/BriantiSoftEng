98d46d36c06fe40d3913b17ad39b49607cf7d222
jtkardon <jtkardon@gmail.com>
2023-04-15 17:41:02 -0400

    typos

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -1,11 +1,9 @@
 package edu.wpi.teamname;
 
 import edu.wpi.teamname.database.DataManager;
-import edu.wpi.teamname.navigation.LocationName;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.text.ParseException;
-import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) throws SQLException, IOException, ParseException {
