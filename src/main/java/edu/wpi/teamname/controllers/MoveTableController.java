b03935b878f0a15fd56dd6ebe6c77fdfde326dd0
Adelynn Martin <Addy>
2023-04-16 04:59:44 -0400

    add fields complete

--- src/main/java/edu/wpi/teamname/controllers/MoveTableController.java
+++ src/main/java/edu/wpi/teamname/controllers/MoveTableController.java
@@ -3,18 +3,23 @@ package edu.wpi.teamname.controllers;
 import edu.wpi.teamname.database.DataManager;
 import edu.wpi.teamname.database.MoveDAOImpl;
 import edu.wpi.teamname.navigation.Move;
+import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.FileChooser;
-import jdk.jfr.Timestamp;
+
 
 import java.io.File;
 import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.sql.Timestamp;
 
 public class MoveTableController {
     @FXML
@@ -24,6 +29,11 @@ public class MoveTableController {
 
     @FXML
     private Button exportButton;
+    @FXML private TextField nodeIdTextField;
+    @FXML private TextField longNameTextField;
+    @FXML private TextField dateTextField;
+    @FXML private MFXButton submitButton;
+
 
     public void initialize() {
         TableColumn<Move, Integer> nodeIDColumn = new TableColumn<>("Node ID");
@@ -79,6 +89,19 @@ public class MoveTableController {
                 } catch (SQLException e) {
                     System.err.println("Error exporting data to CSV file: " + e.getMessage());
                 }
+            }
+        });
+        submitButton.setOnAction(event -> {
+            int nodeId = Integer.parseInt(nodeIdTextField.getText());
+            String longName = longNameTextField.getText();
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            LocalDateTime dateTime = LocalDateTime.parse(dateTextField.getText(), formatter);
+            Timestamp date = Timestamp.valueOf(dateTime);
+            Move move = new Move(nodeId, longName, date);
+            try {
+                moveDAO.addMoves(move);
+            } catch (SQLException e) {
+                e.printStackTrace();
             }
         });
     }
