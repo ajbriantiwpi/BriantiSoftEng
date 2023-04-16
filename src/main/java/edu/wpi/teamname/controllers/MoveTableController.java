5e5393d63a52892a1508d2104f0c9c3cc47c6d62
Adelynn Martin <Addy>
2023-04-16 05:46:09 -0400

    move Table loads and included button on home page

--- src/main/java/edu/wpi/teamname/controllers/MoveTableController.java
+++ src/main/java/edu/wpi/teamname/controllers/MoveTableController.java
@@ -4,6 +4,12 @@ import edu.wpi.teamname.database.DataManager;
 import edu.wpi.teamname.database.MoveDAOImpl;
 import edu.wpi.teamname.navigation.Move;
 import io.github.palexdev.materialfx.controls.MFXButton;
+import java.io.File;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -13,96 +19,87 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.FileChooser;
 
-
-import java.io.File;
-import java.sql.SQLException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.sql.Timestamp;
-
 public class MoveTableController {
-    @FXML
-    private TableView<Move> moveTable;
-    @FXML
-    private Button importButton;
+  @FXML private TableView<Move> moveTable;
+  @FXML private Button importButton;
 
-    @FXML
-    private Button exportButton;
-    @FXML private TextField nodeIdTextField;
-    @FXML private TextField longNameTextField;
-    @FXML private TextField dateTextField;
-    @FXML private MFXButton submitButton;
+  @FXML private Button exportButton;
+  @FXML private TextField nodeIdTextField;
+  @FXML private TextField longNameTextField;
+  @FXML private TextField dateTextField;
+  @FXML private MFXButton submitButton;
 
+  public void initialize() {
+    TableColumn<Move, Integer> nodeIDColumn = new TableColumn<>("Node ID");
+    nodeIDColumn.setCellValueFactory(new PropertyValueFactory<>("nodeID"));
 
-    public void initialize() {
-        TableColumn<Move, Integer> nodeIDColumn = new TableColumn<>("Node ID");
-        nodeIDColumn.setCellValueFactory(new PropertyValueFactory<>("nodeID"));
+    TableColumn<Move, String> longNameColumn = new TableColumn<>("Long Name");
+    longNameColumn.setCellValueFactory(new PropertyValueFactory<>("longName"));
 
-        TableColumn<Move, String> longNameColumn = new TableColumn<>("Long Name");
-        longNameColumn.setCellValueFactory(new PropertyValueFactory<>("longName"));
+    TableColumn<Move, Timestamp> dateColumn = new TableColumn<>("Date");
+    dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
 
-        TableColumn<Move, Timestamp> dateColumn = new TableColumn<>("Date");
-        dateColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+    moveTable.getColumns().addAll(nodeIDColumn, longNameColumn, dateColumn);
 
-        moveTable.getColumns().addAll(nodeIDColumn, longNameColumn, dateColumn);
+    DataManager moveDAO = new DataManager();
 
-        DataManager moveDAO = new DataManager();
-
-        try {
-            ArrayList<Move> moves = moveDAO.getAllMoves();
-            moveTable.setItems(FXCollections.observableArrayList(moves));
-        } catch (SQLException e) {
-            System.err.println("Error getting moves from database: " + e.getMessage());
-        }
-        importButton.setOnAction(event -> {
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("Select CSV File");
-            fileChooser.getExtensionFilters().addAll(
-                    new FileChooser.ExtensionFilter("CSV Files", "*.csv")
-            );
-            File file = fileChooser.showOpenDialog(importButton.getScene().getWindow());
-            if (file != null) {
-                try {
-                    MoveDAOImpl.uploadMoveToPostgreSQL(file.getAbsolutePath());
-                    // refresh table view
-                    moveTable.getItems().clear();
-                    moveTable.getItems().addAll(moveDAO.getAllMoves());
-                } catch (SQLException e) {
-                    System.err.println("Error uploading data to database: " + e.getMessage());
-                }
-            }
-        });
-
-        // event handler for export button
-        exportButton.setOnAction(event -> {
-            FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("Save CSV File");
-            fileChooser.setInitialFileName("moves.csv");
-            fileChooser.getExtensionFilters().addAll(
-                    new FileChooser.ExtensionFilter("CSV Files", "*.csv")
-            );
-            File file = fileChooser.showSaveDialog(exportButton.getScene().getWindow());
-            if (file != null) {
-                try {
-                    MoveDAOImpl.exportMoveToCSV(file.getAbsolutePath());
-                } catch (SQLException e) {
-                    System.err.println("Error exporting data to CSV file: " + e.getMessage());
-                }
-            }
-        });
-        submitButton.setOnAction(event -> {
-            int nodeId = Integer.parseInt(nodeIdTextField.getText());
-            String longName = longNameTextField.getText();
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            LocalDateTime dateTime = LocalDateTime.parse(dateTextField.getText(), formatter);
-            Timestamp date = Timestamp.valueOf(dateTime);
-            Move move = new Move(nodeId, longName, date);
-            try {
-                moveDAO.addMoves(move);
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        });
+    try {
+      ArrayList<Move> moves = moveDAO.getAllMoves();
+      moveTable.setItems(FXCollections.observableArrayList(moves));
+    } catch (SQLException e) {
+      System.err.println("Error getting moves from database: " + e.getMessage());
     }
+    importButton.setOnAction(
+        event -> {
+          FileChooser fileChooser = new FileChooser();
+          fileChooser.setTitle("Select CSV File");
+          fileChooser
+              .getExtensionFilters()
+              .addAll(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
+          File file = fileChooser.showOpenDialog(importButton.getScene().getWindow());
+          if (file != null) {
+            try {
+              MoveDAOImpl.uploadMoveToPostgreSQL(file.getAbsolutePath());
+              // refresh table view
+              moveTable.getItems().clear();
+              moveTable.getItems().addAll(moveDAO.getAllMoves());
+            } catch (SQLException e) {
+              System.err.println("Error uploading data to database: " + e.getMessage());
+            }
+          }
+        });
+
+    // event handler for export button
+    exportButton.setOnAction(
+        event -> {
+          FileChooser fileChooser = new FileChooser();
+          fileChooser.setTitle("Save CSV File");
+          fileChooser.setInitialFileName("moves.csv");
+          fileChooser
+              .getExtensionFilters()
+              .addAll(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
+          File file = fileChooser.showSaveDialog(exportButton.getScene().getWindow());
+          if (file != null) {
+            try {
+              MoveDAOImpl.exportMoveToCSV(file.getAbsolutePath());
+            } catch (SQLException e) {
+              System.err.println("Error exporting data to CSV file: " + e.getMessage());
+            }
+          }
+        });
+    submitButton.setOnAction(
+        event -> {
+          int nodeId = Integer.parseInt(nodeIdTextField.getText());
+          String longName = longNameTextField.getText();
+          DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+          LocalDateTime dateTime = LocalDateTime.parse(dateTextField.getText(), formatter);
+          Timestamp date = Timestamp.valueOf(dateTime);
+          Move move = new Move(nodeId, longName, date);
+          try {
+            moveDAO.addMoves(move);
+          } catch (SQLException e) {
+            e.printStackTrace();
+          }
+        });
+  }
 }
