f894fb91226940b46433d16aa545ddd4e5b061b5
geniusturtle3 <spmarkwick@wpi.edu>
2023-04-21 15:33:28 -0400

    SOF-257 #In-Progress working sexual print prototype

--- src/main/java/edu/wpi/teamname/navigation/Map.java
+++ src/main/java/edu/wpi/teamname/navigation/Map.java
@@ -48,6 +48,7 @@ public class Map {
 
   /**
    * Constructs a Map object with the given sub-anchor pane.
+   *
    * @param subAnchor the sub-anchor pane used to display the map.
    * @throws SQLException if there is an error accessing the database.
    */
@@ -59,6 +60,7 @@ public class Map {
 
   /**
    * Creates and returns an ArrayList of all nodes and edges on the given floor.
+   *
    * @param floor the floor to retrieve nodes and edges from.
    * @param isMapPage whether or not the map is currently being displayed.
    * @return an ArrayList of all nodes and edges on the given floor as JavaFX Node objects.
@@ -78,6 +80,7 @@ public class Map {
 
   /**
    * Creates and returns an ArrayList of all edges on the given floor.
+   *
    * @param floor the floor to retrieve edges from.
    * @param isMapPage whether or not the map is currently being displayed.
    * @return an ArrayList of all edges on the given floor as JavaFX Node objects.
@@ -465,11 +468,44 @@ public class Map {
 
     shapes = makeShapePath(nodePath);
 
+    getTextual(nodePath);
+
     // return shapes
 
     //    System.out.println(nodePath);
 
     // parent.getChildren().addAll(shapes);
+  }
+
+  public void /*ArrayList<String>*/ getTextual(ArrayList<Node> nodePath) {
+    if (nodePath.size() > 2) {
+      for (int i = 1; i < nodePath.size() - 1; i++) {
+        Node prevNode = nodePath.get(i - 1);
+        Node node = nodePath.get(i);
+        // y's are flipped I think since +y is down
+        Node nextNode = nodePath.get(i + 1);
+        double angPrev = Math.atan2(prevNode.getY() - node.getY(), node.getX() - prevNode.getX());
+        double angNext = Math.atan2(nextNode.getY() - node.getY(), node.getX() - nextNode.getX());
+        double angDelta = angNext - angPrev;
+        // adjust over turn
+        if (angDelta > Math.PI) {
+          angDelta -= 2 * Math.PI;
+        } else if (angDelta < -Math.PI) {
+          angDelta += 2 * Math.PI;
+        }
+        System.out.println(node.getId());
+        System.out.println(angDelta);
+        if ((angDelta > 7 * Math.PI / 8) || (angDelta < -7 * Math.PI / 8)) {
+          System.out.println("Straight");
+        } else if (angDelta > Math.PI / 8) {
+          System.out.println("Right");
+        } else if (angDelta < Math.PI / 8) {
+          System.out.println("Left");
+        } else {
+          System.out.println("Turn Around");
+        }
+      }
+    }
   }
 
   /**
