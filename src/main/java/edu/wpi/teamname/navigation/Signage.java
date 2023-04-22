69ac25912d62561455646723612cce3088633c06
Adelynn Martin <Addy>
2023-04-22 10:50:36 -0400

    signage class done

--- src/main/java/edu/wpi/teamname/navigation/Signage.java
+++ src/main/java/edu/wpi/teamname/navigation/Signage.java
@@ -18,4 +18,37 @@ public class Signage {
         this.date=date;
         this.arrowDirection=arrowDirection;
     }
+
+    /**
+     * Returns a string representation of this Signage object.
+     *
+     * @return a string representation of this Signage object
+     */
+    @Override
+    public String toString() {
+        return "[" + longName + ", " + shortName + ", " + date + ", " + arrowDirection + "]";
+    }
+
+    /**
+     * Compares this Signage object to another Signage object for equality.
+     *
+     * @param other the Signage object to compare to
+     * @return true if the two objects are equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof Signage)) {
+            return false;
+        }
+        Signage otherSignage = (Signage) other;
+        return this.longName.equals(otherSignage.getLongName())
+                && this.shortName.equals(otherSignage.getShortName())
+                && this.date.equals(otherSignage.getDate())
+                && this.arrowDirection.equals(otherSignage.getArrowDirection());
+    }
+
+
 }
