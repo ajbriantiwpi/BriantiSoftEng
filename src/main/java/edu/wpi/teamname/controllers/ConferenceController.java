6fbb03a197439941e1e5ca6446f9c0b7625bf34d
Hunter102 <40743716+Hunter102@users.noreply.github.com>
2023-04-22 14:21:16 -0400

    confroom stuff 3

--- src/main/java/edu/wpi/teamname/controllers/ConferenceController.java
+++ src/main/java/edu/wpi/teamname/controllers/ConferenceController.java
@@ -1,4 +1,28 @@
 package edu.wpi.teamname.controllers;
 
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.layout.VBox;
+import org.controlsfx.control.RangeSlider;
+
 public class ConferenceController {
+
+    @FXML ComboBox startBox;
+    @FXML ComboBox endBox;
+    @FXML ComboBox buildingBox;
+    @FXML ComboBox roomBox;
+    @FXML DatePicker dateBox;
+    @FXML RangeSlider sizeSlider;
+    @FXML MFXButton submitButton;
+    @FXML VBox viewBox;
+
+
+
+
+    @FXML
+    public void initialize(){
+
+    }
 }
