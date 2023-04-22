c650f67f4acf4f9eadbf885ec4bcfb0a8ea703a9
Hunter102 <40743716+Hunter102@users.noreply.github.com>
2023-04-22 15:03:54 -0400

    fxml

--- src/main/java/edu/wpi/teamname/controllers/ConferenceController.java
+++ src/main/java/edu/wpi/teamname/controllers/ConferenceController.java
@@ -4,6 +4,8 @@ import io.github.palexdev.materialfx.controls.MFXButton;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.layout.VBox;
 import org.controlsfx.control.RangeSlider;
 
@@ -17,6 +19,33 @@ public class ConferenceController {
     @FXML RangeSlider sizeSlider;
     @FXML MFXButton submitButton;
     @FXML VBox viewBox;
+    //----------TABLE FXML-----------
+    //@FXML TableView<ConfRoom> confTable;
+    @FXML TableColumn roomName;
+    @FXML TableColumn am8;
+    @FXML TableColumn am830;
+    @FXML TableColumn am9;
+    @FXML TableColumn am930;
+    @FXML TableColumn am10;
+    @FXML TableColumn am1030;
+    @FXML TableColumn am11;
+    @FXML TableColumn am1130;
+    @FXML TableColumn pm12;
+    @FXML TableColumn pm1230;
+    @FXML TableColumn pm1;
+    @FXML TableColumn pm130;
+    @FXML TableColumn pm2;
+    @FXML TableColumn pm230;
+    @FXML TableColumn pm3;
+    @FXML TableColumn pm330;
+    @FXML TableColumn pm4;
+    @FXML TableColumn pm430;
+    @FXML TableColumn pm5;
+    @FXML TableColumn pm530;
+    @FXML TableColumn pm6;
+    @FXML TableColumn pm630;
+
+    //---------------------
 
 
 
