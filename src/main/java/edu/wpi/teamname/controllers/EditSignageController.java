515af653635eb22ff1724d8095d2719a2e076af3
Adelynn Martin <Addy>
2023-04-23 14:29:57 -0400

    signage delete done

--- src/main/java/edu/wpi/teamname/controllers/EditSignageController.java
+++ src/main/java/edu/wpi/teamname/controllers/EditSignageController.java
@@ -1,7 +1,9 @@
 package edu.wpi.teamname.controllers;
 
+import edu.wpi.teamname.database.DataManager;
 import edu.wpi.teamname.database.SignageDAOImpl;
 import edu.wpi.teamname.database.interfaces.SignageDAO;
+import edu.wpi.teamname.employees.Employee;
 import edu.wpi.teamname.navigation.Direction;
 import edu.wpi.teamname.navigation.Signage;
 import io.github.palexdev.materialfx.controls.MFXButton;
@@ -9,19 +11,24 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
 import jdk.jfr.Timestamp;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.util.List;
+import java.util.Optional;
 
 
 import static edu.wpi.teamname.database.DataManager.syncSignage;
@@ -51,6 +58,7 @@ public class EditSignageController {
     private AnchorPane rootPane;
 
     public void initialize() {
+        DataManager signageDAO=new DataManager();
         TableColumn<Signage, String> longNameColumn = new TableColumn<>("Long Name");
         longNameColumn.setCellValueFactory(new PropertyValueFactory<>("longName"));
 
@@ -73,6 +81,9 @@ public class EditSignageController {
         longNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
         shortNameColumn.setCellFactory(TextFieldTableCell.forTableColumn());
         arrowDirectionColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        ContextMenu contextMenu = new ContextMenu();
+        MenuItem deleteMenuItem = new MenuItem("Delete");
+        contextMenu.getItems().add(deleteMenuItem);
 
         // Make the TableView editable
         editSignageTable.setEditable(true);
@@ -128,6 +139,45 @@ public class EditSignageController {
                 throw new RuntimeException(e);
             }
         });
+        deleteMenuItem.setOnAction(event -> deleteSelectedSignage());
+        exportButton.setOnAction(
+                event -> {
+                    FileChooser fileChooser = new FileChooser();
+                    fileChooser.setTitle("Save CSV File");
+                    fileChooser.setInitialFileName("signage.csv");
+                    fileChooser
+                            .getExtensionFilters()
+                            .addAll(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
+                    File file = fileChooser.showSaveDialog(exportButton.getScene().getWindow());
+                    if (file != null) {
+                        try {
+                            signageDAO.exportSignageToCSV(file.getAbsolutePath());
+                        } catch (SQLException | IOException e) {
+                            System.err.println("Error exporting data to CSV file: " + e.getMessage());
+                        }
+                    }
+                });
+        importButton.setOnAction(
+                event -> {
+                    FileChooser fileChooser = new FileChooser();
+                    fileChooser.setTitle("Select CSV File");
+                    fileChooser
+                            .getExtensionFilters()
+                            .addAll(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
+                    File file = fileChooser.showOpenDialog(importButton.getScene().getWindow());
+                    if (file != null) {
+                        try {
+                            signageDAO.uploadSignage(file.getAbsolutePath());
+                            // refresh table view
+                            signageDAO.getAllSignage().clear();
+                            editSignageTable.getItems().addAll(signageDAO.getAllSignage());
+                        } catch (SQLException e) {
+                            System.err.println("Error uploading data to database: " + e.getMessage());
+                        } catch (ParseException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                });
 
         editSignageTable.getColumns().addAll(longNameColumn, shortNameColumn, dateColumn, arrowDirectionColumn, signIDColumn, kioskIDColumn);
         loadData();
@@ -172,4 +222,38 @@ public class EditSignageController {
         kioskInput.clear();
         directionPicker.setValue(null);
     }
+    private void deleteSelectedSignage() {
+        DataManager signageDAO=new DataManager();
+       Signage selectedSignage = editSignageTable.getSelectionModel().getSelectedItem();
+        if (selectedSignage != null) {
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("Delete Signage");
+            alert.setHeaderText("Are you sure you want to delete this signage?");
+            alert.setContentText(
+                    "Sign ID: "
+                            + selectedSignage.getSignId()
+                            + "\nLong Name: "
+                            + selectedSignage.getLongName()
+                            + "\nShort Name: "
+                            + selectedSignage.getShortName()
+                            + "\nArrow Direction: "
+                            + selectedSignage.getArrowDirection()
+                            + "\nDate: "
+                            + selectedSignage.getDate()
+                            + "\nKiosk ID: "
+                            + selectedSignage.getKioskId());
+
+            Optional<ButtonType> result = alert.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                try {
+                    signageDAO.deleteSignage(selectedSignage);
+                    // employeeDAO.deleteEmployeeType(selectedEmployee.getUsername());
+                    editSignageTable.getItems().remove(selectedSignage);
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
 }
