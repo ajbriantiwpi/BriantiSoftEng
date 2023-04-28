37ce03db971be6187b88e9d3f8a692810ce21bf5
C-Slat <ctslattery@wpi.edu>
2023-04-28 19:24:10 -0400

    popups work

--- src/main/java/edu/wpi/teamname/controllers/AboutPageController.java
+++ src/main/java/edu/wpi/teamname/controllers/AboutPageController.java
@@ -1,18 +1,14 @@
 package edu.wpi.teamname.controllers;
 
-import static edu.wpi.teamname.Screen.*;
-
-import edu.wpi.teamname.Navigation;
 import io.github.palexdev.materialfx.controls.MFXButton;
 import java.io.IOException;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
+import org.controlsfx.control.PopOver;
 
 public class AboutPageController {
 
@@ -31,82 +27,100 @@ public class AboutPageController {
   @FXML AnchorPane aboutAnchorPane;
 
   public void initialize() {
+    MFXButton.class.getClassLoader().setDefaultAssertionStatus(false);
+
+    EventHandler<MouseEvent> PersonPopup =
+        new EventHandler<MouseEvent>() {
+          @Override
+          public void handle(MouseEvent event) {
+            MFXButton clickedButton = (MFXButton) event.getSource();
+            String fxmlFileName = null;
+
+            switch (clickedButton.getId()) {
+              case "ianButton":
+                fxmlFileName = "IanPopup.fxml";
+                break;
+              case "jasonButton":
+                fxmlFileName = "JasonPopup.fxml";
+                break;
+              case "alessandroButton":
+                fxmlFileName = "AlessandroPopup.fxml";
+                break;
+              case "adelynnButton":
+                fxmlFileName = "AddyPopup.fxml";
+                break;
+              case "aleksandrButton":
+                fxmlFileName = "AlekPopup.fxml";
+                break;
+              case "samuelButton":
+                fxmlFileName = "SamPopup.fxml";
+                break;
+              case "ryanButton":
+                fxmlFileName = "RyanPopup.fxml";
+                break;
+              case "conorButton":
+                fxmlFileName = "ConorPopup.fxml";
+                break;
+              case "hunterButton":
+                fxmlFileName = "HunterPopup.fxml";
+                break;
+              case "arturoButton":
+                fxmlFileName = "ArturoPopup.fxml";
+                break;
+              case "andrewButton":
+                fxmlFileName = "AndrewPopup.fxml";
+                break;
+              default:
+                break;
+            }
+
+            if (fxmlFileName != null) {
+              final var resource =
+                  getClass().getResource("/edu/wpi/teamname/views/" + fxmlFileName);
+              final FXMLLoader loader = new FXMLLoader(resource);
+              VBox popupContent;
+
+              try {
+                popupContent = loader.load();
+              } catch (IOException e) {
+                throw new RuntimeException(e);
+              }
+
+              PopOver popOver = new PopOver(popupContent);
+              popOver.show(clickedButton);
+            }
+          }
+        };
+
+    // Set the event handler on each button
+    ianButton.setOnMouseClicked(PersonPopup);
+    jasonButton.setOnMouseClicked(PersonPopup);
+    alessandroButton.setOnMouseClicked(PersonPopup);
+    adelynnButton.setOnMouseClicked(PersonPopup);
+    aleksandrButton.setOnMouseClicked(PersonPopup);
+    samuelButton.setOnMouseClicked(PersonPopup);
+    ryanButton.setOnMouseClicked(PersonPopup);
+    conorButton.setOnMouseClicked(PersonPopup);
+    hunterButton.setOnMouseClicked(PersonPopup);
+    arturoButton.setOnMouseClicked(PersonPopup);
+    andrewButton.setOnMouseClicked(PersonPopup);
+
+    // Set the event handler for each button and their associated FXML file
+  }
+}
 
     /**
-     * final var resource = App.class.getResource("ConorPopup.fxml"); final FXMLLoader loader = new
+     * EventHandler<MouseEvent> PersonPopup = new EventHandler<MouseEvent>() { @Override public void
+     * handle(MouseEvent event) {
+     *
+     * <p>MFXButton createNewButton = ((MFXButton) event.getSource());
+     *
+     * <p>final var resource = App.class.getResource("IanPopup.fxml"); final FXMLLoader loader = new
      * FXMLLoader(resource); VBox v;
      *
-     * <p>try { v = loader.load(); } catch (IOException e) { throw new RuntimeException(e); }
-     * PopOver pop = new PopOver(v); *
+     * <p>try { v = loader.load(); } catch ( IOException e) { throw new RuntimeException(e); }
+     *
+     * <p>PopOver pop = new PopOver(v);
+     *
+     * <p>pop.show(createNewButton); } }; } } *
      */
-    conorButton.setOnMouseClicked(
-        event -> Navigation.navigate(CONOR_POPUP)); // change this to popup
-    ianButton.setOnMouseClicked(event -> Navigation.navigate(IAN_POPUP)); // change this to popup
-    jasonButton.setOnMouseClicked(
-        event -> Navigation.navigate(JASON_POPUP)); // change this to popup
-    alessandroButton.setOnMouseClicked(
-        event -> Navigation.navigate(ALESSANDRO_POPUP)); // change this to popup
-    adelynnButton.setOnMouseClicked(
-        event -> Navigation.navigate(ADDY_POPUP)); // change this to popup
-    aleksandrButton.setOnMouseClicked(
-        event -> Navigation.navigate(ALEK_POPUP)); // change this to popup
-    samuelButton.setOnMouseClicked(event -> Navigation.navigate(SAM_POPUP)); // change this to popup
-    ryanButton.setOnMouseClicked(event -> Navigation.navigate(RYAN_POPUP)); // change this to popup
-    hunterButton.setOnMouseClicked(
-        event -> Navigation.navigate(HUNTER_POPUP)); // change this to popup
-    arturoButton.setOnMouseClicked(
-        event -> Navigation.navigate(ARTURO_POPUP)); // change this to popup
-    andrewButton.setOnMouseClicked(
-        event -> Navigation.navigate(ANDREW_POPUP)); // change this to popup
-  }
-
-  private void showPopupForTeamMember(String fxmlFile, ImageView imageView) {
-    // Load and show the popup for the selected team member
-    try {
-      FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
-      Parent root = loader.load();
-      Stage stage = new Stage();
-      stage.setScene(new Scene(root));
-      stage.show();
-
-      // Position the popup next to the clicked ImageView
-      stage.setX(imageView.localToScreen(imageView.getBoundsInLocal()).getMaxX());
-      stage.setY(imageView.localToScreen(imageView.getBoundsInLocal()).getMinY());
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private void showPopupForTeamMember(String fxmlFile) {
-    // Load and show the popup for the selected team member
-    try {
-      FXMLLoader loader = new FXMLLoader(getClass().getResource(fxmlFile));
-      Parent root = loader.load();
-      Stage stage = new Stage();
-      stage.setScene(new Scene(root));
-      stage.show();
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
-  private void hidePopup() {
-    // Close the current popup (if one is open)
-    if (currentPopup != null) {
-      currentPopup.close();
-      currentPopup = null;
-    }
-  }
-
-  // Define a field to store the currently open popup
-  private Stage currentPopup;
-}
-/**
- * aboutAnchorPane.setOnMouseEntered( event -> { // Check if the mouse click event occurred on one
- * of the ImageView controls if (event.getTarget() == conorButton) { // Show the popup for team
- * member 1 showPopupForTeamMember("ConorPopup.fxml"); } else if (event.getTarget() == ianButton) {
- * // Show the popup for team member 2 showPopupForTeamMember("IanPopup.fxml"); } else if
- * (event.getTarget() == jasonButton) { // Show the popup for team member 3
- * showPopupForTeamMember("JasonPopup.fxml"); } // Add more else if statements for other team
- * members }); aboutAnchorPane.setOnMouseExited( event -> { hidePopup(); }); } *
- */
