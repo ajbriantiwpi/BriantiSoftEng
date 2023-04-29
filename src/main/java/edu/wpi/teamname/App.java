02749f4ae4168f281ec80001af495949bdd044b0
geniusturtle3 <spmarkwick@wpi.edu>
2023-04-28 20:12:54 -0400

    It displays

--- src/main/java/edu/wpi/teamname/App.java
+++ src/main/java/edu/wpi/teamname/App.java
@@ -45,7 +45,7 @@ public class App extends Application {
     primaryStage.setScene(scene);
     primaryStage.show();
 
-    Navigation.navigate(Screen.HOME);
+    Navigation.navigate(Screen.CONF_VIEW);
   }
 
   @Override
