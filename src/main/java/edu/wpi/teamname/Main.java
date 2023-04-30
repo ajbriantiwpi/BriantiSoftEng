5f62c697b00493adfb0e9ea971512e7ec7db8b1d
jtkardon <jtkardon@gmail.com>
2023-04-30 16:25:59 -0400

    took employee out of main

--- src/main/java/edu/wpi/teamname/Main.java
+++ src/main/java/edu/wpi/teamname/Main.java
@@ -1,13 +1,11 @@
 package edu.wpi.teamname;
 
-import edu.wpi.teamname.database.DataManager;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.text.ParseException;
 
 public class Main {
   public static void main(String[] args) throws SQLException, IOException, ParseException {
-    GlobalVariables.setCurrentUser(DataManager.getEmployee("ian"));
     App.launch(App.class, args);
   }
   // shortcut: psvm
