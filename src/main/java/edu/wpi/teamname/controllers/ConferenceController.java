476516e937adf52ef9e179d08d3d6aeed9b1a7d2
geniusturtle3 <spmarkwick@wpi.edu>
2023-05-02 20:14:59 -0400

    fixed conference room

--- src/main/java/edu/wpi/teamname/controllers/ConferenceController.java
+++ src/main/java/edu/wpi/teamname/controllers/ConferenceController.java
@@ -36,7 +36,7 @@ public class ConferenceController {
   @FXML Label roomSizeLabel;
   @FXML Label nameLabel;
   @FXML Label roomLabel;
-  @FXML AnchorPane root;
+  @FXML AnchorPane rootPane;
   @FXML ComboBox<String> startBox;
   @FXML ComboBox<String> endBox;
   @FXML ComboBox<String> buildingBox;
@@ -158,12 +158,7 @@ public class ConferenceController {
         (options, oldValue, newValue) -> {
           setLanguage(newValue);
         });
-    if (GlobalVariables.getDarkMode().get()) {
-      root.getStylesheets().remove(0);
-    } else {
-      root.getStylesheets().remove(1);
-    }
-    ThemeSwitch.switchTheme(root);
+    ThemeSwitch.switchTheme(rootPane);
     buildings = FXCollections.observableArrayList(DataManager.getConfBuildings());
     buildings.add("None");
     roomsString = FXCollections.observableArrayList();
